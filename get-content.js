9693878034404f975e5fd2b8ba735acc10cea05f
Alejandro Sanchez <alejandro.sanchez@porsche.digital>
2024-03-03 14:23:21 +0100

    adding linter to the project

--- get-content.js
+++ get-content.js
@@ -1,94 +1,85 @@
-let htmlToCopy = "";
+let htmlToCopy = ''
 
 async function getContent() {
-  let [tab] = await chrome.tabs.query({ active: true });
+  const [tab] = await chrome.tabs.query({ active: true })
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: () => {
       function getImageName(element) {
-        const path = element.currentSrc || element.attributes.srcset?.nodeValue;
+        const path = element.currentSrc || element.attributes.srcset?.nodeValue
 
-        const questionMarkPosition = path.lastIndexOf("?");
-        const lastUnderscorePosition = path.lastIndexOf("_") + 1;
+        const questionMarkPosition = path.lastIndexOf('?')
+        const lastUnderscorePosition = path.lastIndexOf('_') + 1
 
-        const shortPath = path.slice(lastUnderscorePosition, questionMarkPosition);
+        const shortPath = path.slice(lastUnderscorePosition, questionMarkPosition)
 
-        console.log("shortPath", shortPath);
-        return shortPath;
+        console.log('shortPath', shortPath)
+        return shortPath
       }
 
       function createNewImage(element) {
-        const elementEl = document.createElement("div");
-        elementEl.style.border = "1px solid black";
+        const elementElement = document.createElement('div')
+        elementElement.style.border = '1px solid black'
 
-        const imageName = getImageName(element);
+        const imageName = getImageName(element)
 
-        imageNameEl = document.createElement("p");
-        imageNameEl.textContent = `Image name: ${imageName}`;
+        const imageNameElement = document.createElement('p')
+        imageNameElement.textContent = `Image name: ${imageName}`
 
-        imageAltEl = document.createElement("p");
-        imageAltEl.textContent = `Image alt: ${element.alt}`;
+        const imageAltElement = document.createElement('p')
+        imageAltElement.textContent = `Image alt: ${element.alt}`
 
-        elementEl.append(imageNameEl, imageAltEl);
+        elementElement.append(imageNameElement, imageAltElement)
 
-        return elementEl;
+        return elementElement
       }
 
       function createOtherHtmlElements(element) {
-        elementEl = document.createElement(element.tagName);
-        elementEl.textContent = element.textContent;
+        const elementElement = document.createElement(element.tagName)
+        elementElement.textContent = element.textContent
 
-        return elementEl;
+        return elementElement
       }
 
       function createNewElement(element) {
-        let elementEl;
+        const elementElement = element.tagName === 'IMG' ? createNewImage(element) : createOtherHtmlElements(element)
 
-        if (element.tagName === "IMG") {
-          elementEl = createNewImage(element);
-        } else {
-          elementEl = createOtherHtmlElements(element);
+        const elementStyle = getComputedStyle(element)
+
+        elementElement.style = {
+          ...elementStyle
         }
-
-        const elementStyle = getComputedStyle(element);
-
-        elementEl.style = {
-          ...elementStyle,
-        };
-
-        return elementEl;
+        return elementElement
       }
 
-      const newHTML = document.createElement("div");
+      const newHTML = document.createElement('div')
       const selectorAllElementsInsideMain =
-        "main h2:not(.conversion h2), main p:not(.conversion p), main img:not(.conversion img)";
+        'main h2:not(.conversion h2), main p:not(.conversion p), main img:not(.conversion img)'
 
-      const allElements = document.querySelectorAll(selectorAllElementsInsideMain);
-      const elementsInfo = [...allElements].forEach((element) => {
-        let elementEl = createNewElement(element);
-        if (elementEl) {
-          newHTML.appendChild(elementEl);
-          console.log("nreHTML", newHTML);
+      const allElements = document.querySelectorAll(selectorAllElementsInsideMain)
+      for (const element of allElements) {
+        const elementElement = createNewElement(element)
+        if (elementElement) {
+          newHTML.append(elementElement)
+          console.log('nreHTML', newHTML)
         }
-      });
-
-      console.log("elements from extension", elementsInfo);
-      htmlToCopy = newHTML.innerHTML;
-      console.log("htmlToCopy", htmlToCopy);
-
-      function listener(e) {
-        e.clipboardData.setData("text/html", htmlToCopy);
-        e.clipboardData.setData("text/plain", htmlToCopy);
-        e.preventDefault();
       }
-      document.addEventListener("copy", listener);
-      document.execCommand("copy");
-      document.removeEventListener("copy", listener);
 
-      alert("Content copied to clipboard");
-    },
-  });
+      htmlToCopy = newHTML.innerHTML
+      console.log('htmlToCopy', htmlToCopy)
+
+      function listener(event) {
+        event.clipboardData.setData('text/html', htmlToCopy)
+        event.clipboardData.setData('text/plain', htmlToCopy)
+        event.preventDefault()
+      }
+      document.addEventListener('copy', listener)
+      document.execCommand('copy')
+      document.removeEventListener('copy', listener)
+
+      alert('Content copied to clipboard')
+    }
+  })
 }
 
-document.getElementById("myContent").addEventListener("click", getContent);
-
+document.querySelector('#myContent').addEventListener('click', getContent)
--- reference.js
+++ reference.js
@@ -1,45 +1,45 @@
 async function getImages() {
-    let [tab] = await chrome.tabs.query({ active: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: () => {
-        const images = [...document.querySelectorAll("img.image__img")];
-        const imagesInfo = images.map((image) => ({
-          src: image.attributes.srcset.nodeValue
-            ? image.attributes.srcset.nodeValue
-            : image.currentSrc,
-          alt: image.alt,
-        }));
-        console.log("images from extension", imagesInfo);
-      },
-    });
-  }
-
-  function copyToClip(str) {
-    function listener(e) {
-      e.clipboardData.setData("text/html", str);
-      e.clipboardData.setData("text/plain", str);
-      e.preventDefault();
+  const [tab] = await chrome.tabs.query({ active: true })
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      const images = [...document.querySelectorAll('img.image__img')]
+      const imagesInfo = images.map((image) => ({
+        src: image.attributes.srcset.nodeValue
+          ? image.attributes.srcset.nodeValue
+          : image.currentSrc,
+        alt: image.alt
+      }))
+      console.log('images from extension', imagesInfo)
     }
-    document.addEventListener("copy", listener);
-    document.execCommand("copy");
-    document.removeEventListener("copy", listener);
-  }
-  
-  async function copyContent(stringy, events) {
-    console.log("stringy", stringy, "events", events);
-    let [tab] = await chrome.tabs.query({ active: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: (stringy, events) => {
-        console.log("event", events, "str", stringy);
-        //   copyToClip(str);
-      },
-    });
-  }
+  })
+}
 
-//this may be deleted later
-  document.getElementById("copyContent").addEventListener("click", (event) => {
-    copyContent(htmlToCopy, event);
-  });
-  document.getElementById("myImages").addEventListener("click", getImages);
+function copyToClip(string_) {
+  function listener(e) {
+    e.clipboardData.setData('text/html', string_)
+    e.clipboardData.setData('text/plain', string_)
+    e.preventDefault()
+  }
+  document.addEventListener('copy', listener)
+  document.execCommand('copy')
+  document.removeEventListener('copy', listener)
+}
+
+async function copyContent(stringy, events) {
+  console.log('stringy', stringy, 'events', events)
+  const [tab] = await chrome.tabs.query({ active: true })
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: (stringy, events) => {
+      console.log('event', events, 'str', stringy)
+      //   copyToClip(str);
+    }
+  })
+}
+
+// this may be deleted later
+document.querySelector('#copyContent').addEventListener('click', (event) => {
+  copyContent(htmlToCopy, event)
+})
+document.querySelector('#myImages').addEventListener('click', getImages)
