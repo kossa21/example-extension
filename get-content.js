c2659768dd6bf3e8f1dad3ca90142dc1c2788029
Alejandro Sanchez <alejandro.sanchez@porsche.digital>
2024-03-03 13:39:38 +0100

    separando archivos

--- get-content.js
+++ get-content.js
@@ -1,85 +1,10 @@
-
-
-async function injectButtons() {
-  let [tab] = await chrome.tabs.query({ active: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {
-      const button = document.createElement('button')
-      button.innerText = 'copy alt text'
-      button.classList.add('root')
-
-      const header = document.querySelector("header")
-      const imageHeader =  document.querySelector("header img")
-      header.append(button)
-    },
-  });
-}
-
-
-function copyToClipboard() {
-  var copyText = document.getElementById("copyText");
-  copyText.select();
-  document.execCommand("copy");
-
-  var copyBtn = document.getElementById("copyBtn");
-  copyBtn.innerText = "Copied!";
-  copyBtn.classList.add("copied");
-
-  setTimeout(function () {
-    copyBtn.innerText = "Copy";
-    copyBtn.classList.remove("copied");
-  }, 2000); // Change 2000 to the number of milliseconds you want the "Copied!" message to display
-}
-
-async function getImages() {
-  let [tab] = await chrome.tabs.query({ active: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {
-      const images = [...document.querySelectorAll("img.image__img")];
-      const imagesInfo = images.map((image) => ({
-        src: image.attributes.srcset.nodeValue
-          ? image.attributes.srcset.nodeValue
-          : image.currentSrc,
-        alt: image.alt,
-      }));
-      console.log("images from extension", imagesInfo);
-    },
-  });
-}
-
 let htmlToCopy = "";
-
-function copyToClip(str) {
-  function listener(e) {
-    e.clipboardData.setData("text/html", str);
-    e.clipboardData.setData("text/plain", str);
-    e.preventDefault();
-  }
-  document.addEventListener("copy", listener);
-  document.execCommand("copy");
-  document.removeEventListener("copy", listener);
-}
-
-async function copyContent(stringy, events) {
-  console.log("stringy", stringy, "events", events);
-  let [tab] = await chrome.tabs.query({ active: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: (stringy, events) => {
-      console.log("event", events, "str", stringy);
-      //   copyToClip(str);
-    },
-  });
-}
 
 async function getContent() {
   let [tab] = await chrome.tabs.query({ active: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: () => {
-      console.log("holi from extension");
       function getImageName(element) {
         const path = element.currentSrc || element.attributes.srcset?.nodeValue;
 
@@ -165,13 +90,5 @@ async function getContent() {
   });
 }
 
-// document.getElementById("myButton").addEventListener("click", sayHello);
-document.getElementById("myImages").addEventListener("click", getImages);
 document.getElementById("myContent").addEventListener("click", getContent);
-document.getElementById("copyBtn").addEventListener("click", (e) => {
-  copyToClipboard(e)
-  injectButtons()
-});
-document.getElementById("copyContent").addEventListener("click", (event) => {
-  copyContent(htmlToCopy, event);
-});
+
